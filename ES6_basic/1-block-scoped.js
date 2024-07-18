6488a5315ce156598c44856f55b9ec30fdf64fc0
xhuve <xhuvelialbana@gmail.com>
2024-07-18 17:29:00 +0200

    changes

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+      const task = false;
+      const task2 = true;
   }
 
   return [task, task2];
 }
-
