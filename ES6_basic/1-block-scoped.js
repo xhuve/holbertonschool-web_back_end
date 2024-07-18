b802ab231501127fd4bbe3c8cc0dbf60e5c5718f
xhuve <xhuvelialbana@gmail.com>
2024-07-18 18:32:48 +0200

    Fix 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,10 +2,11 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = false;
     const task2 = true;
   }
 
   return [task, task2];
-}
+}rzcw
