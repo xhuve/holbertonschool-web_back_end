3a44fa4579188cf4ad97e7e1b3237c29576c2b41
xhuve <xhuvelialbana@gmail.com>
2024-07-18 19:06:31 +0200

    Fix vs code eslint problem

--- ES6_basic/eslint.config.mjs
+++ ES6_basic/eslint.config.mjs
@@ -1,35 +1,45 @@
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+
+// Initialize compat to convert eslintrc settings to flat config
+const compat = new FlatCompat({
+  baseDirectory: import.meta.url, // Or __dirname if not using ESM
+});
+
+export default [
+  ...compat.config({
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
     ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
-};
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides: [
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      },
+    ],
+  }),
+];
