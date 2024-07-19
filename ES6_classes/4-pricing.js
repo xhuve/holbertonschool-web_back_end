f53583aa15468bda4c574724c6bfb0952fc52c3e
xhuve <xhuvelialbana@gmail.com>
2024-07-19 16:43:35 +0200

    Finish

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -23,4 +23,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
