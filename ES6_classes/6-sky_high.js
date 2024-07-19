d9a897bcdc04180bc524e5cdd937191d8884ea01
xhuve <xhuvelialbana@gmail.com>
2024-07-19 17:14:36 +0200

    Finish

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,29 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super();
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
