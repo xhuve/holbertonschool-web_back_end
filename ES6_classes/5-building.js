f2b7d085ff1ce58a624b15c11ce31535e7837ca3
xhuve <xhuvelialbana@gmail.com>
2024-07-19 17:04:33 +0200

    Finish

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
