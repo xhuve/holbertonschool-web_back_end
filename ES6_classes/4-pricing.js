1c464599ceda8a003456e036e85e8a668d6c56fe
xhuve <xhuvelialbana@gmail.com>
2024-07-19 16:41:03 +0200

    Finish

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+}
