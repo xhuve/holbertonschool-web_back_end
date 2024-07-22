61fe78291f7b81a7e32efd240bfaa11939975ce1
xhuve <xhuvelialbana@gmail.com>
2024-07-22 19:28:34 +0200

    Finish

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,12 @@
-// eslint-disable-next-line no-undef
 module.exports = function updateStudentGradeByCity(arr, city, newGrades) {
-    return arr.filter((x) => x.location == city)
+  return arr.filter((x) => x.location === city)
     .map((student) => {
-        const newArr = newGrades.filter((grades) => grades.studentId == student.id)[0]
-        
-        // eslint-disable-next-line no-prototype-builtins
-        if (newArr && newArr.hasOwnProperty('grade'))
-            student.grade = newArr.grade
-        else 
-            student.grade = "N/A"
+      const newStudent = student;
+      const newGrade = newGrades.filter((grades) => grades.studentId === student.id)[0];
 
-        return student
-    })
-}
+      // eslint-disable-next-line no-prototype-builtins
+      if (newGrade && newGrade.hasOwnProperty('grade')) { newStudent.grade = newGrade.grade; } else { newStudent.grade = 'N/A'; }
+
+      return newStudent;
+    });
+};
