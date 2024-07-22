26f8dc0d0abcfb1ee534bce2027da483303d72c0
xhuve <xhuvelialbana@gmail.com>
2024-07-22 21:59:57 +0200

    Finish

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 module.exports = function createInt8TypedArray(length, position, value) {
+  if (position > length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const intArray = new Int8Array(buffer, 0, length);
   intArray[position] = value;
