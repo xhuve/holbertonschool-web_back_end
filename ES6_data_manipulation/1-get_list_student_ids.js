36305d9ac492f320e11ad69fa4781e8cdcb58698
xhuve <xhuvelialbana@gmail.com>
2024-07-23 13:04:26 +0200

    Finish

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-undef
-module.exports =  function getListStudentIds(arr) {
-    if (!(arr instanceof Array))
-        return []
+module.exports = function getListStudentIds(arr) {
+  if (!(arr instanceof Array)) { return []; }
 
-    return arr.map((x) => x.id)
-}
+  return arr.map((x) => x.id);
+};
