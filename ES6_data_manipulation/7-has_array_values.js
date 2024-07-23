5d023ddfc4cbde8b64ce479046f611b8db8d53e7
xhuve <xhuvelialbana@gmail.com>
2024-07-23 16:20:54 +0200

    Finish

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,4 @@
 module.exports = function hasValuesFromArray(set, arr) {
   // eslint-disable-next-line no-restricted-syntax
-  for (const value in arr) {
-    if (set.has(Number(value))) { return false; }
-  }
-  return true;
+  return arr.every((value) => set.has(value));
 };
