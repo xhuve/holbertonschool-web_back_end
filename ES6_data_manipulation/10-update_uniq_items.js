7213eafe3de2ad52702d53527d45713e24f42d0b
xhuve <xhuvelialbana@gmail.com>
2024-07-23 16:21:50 +0200

    Finish

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 module.exports = function updateUniqueItems(groceriesMap) {
+  if (groceriesMap instanceof Map) return new Error('Cannot process');
   groceriesMap.forEach((value, key, map) => {
     if (value === 1) {
       map.set(key, 100);
