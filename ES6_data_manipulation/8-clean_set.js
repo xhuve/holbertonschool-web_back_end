9b048c33e978c84ab6cc57b3241cf0eb641895fd
xhuve <xhuvelialbana@gmail.com>
2024-07-23 12:49:44 +0200

    Finish

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,13 @@
 module.exports = function cleanSet(set, startString) {
-    let combinedString = ''
-    if (startString.length == 0)
-        return combinedString
+  let combinedString = '';
+  if (startString.length === 0) { return combinedString; }
 
-    set.forEach(element => {
-        if (element.startsWith(startString)) {
-            let newStr = element.slice(startString.length)
+  set.forEach((element) => {
+    if (element.startsWith(startString)) {
+      const newStr = element.slice(startString.length);
 
-            combinedString = combinedString + newStr + '-'
-        }
-    });
-    return combinedString.slice(0, -1)
-}
+      combinedString = `${combinedString + newStr}-`;
+    }
+  });
+  return combinedString.slice(0, -1);
+};
