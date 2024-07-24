f1d01bd4466512640d61b216fe2ca627fea38077
xhuve <xhuvelialbana@gmail.com>
2024-07-24 13:02:04 +0200

    Finish

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const { uploadPhoto, createUser } = require('./utils');
+
+module.exports = async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(photo.body, user.firstName, user.lastName);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
+};
