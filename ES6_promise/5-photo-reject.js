e17f6a70bb612d1eddda8ac6ed76cd27d4569b71
xhuve <xhuvelialbana@gmail.com>
2024-07-24 15:27:47 +0200

    Finish

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     reject(
       new Error(`${filename} cannot be processed`),
     );
