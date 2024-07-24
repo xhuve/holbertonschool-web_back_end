d7104cb48c373afad25387972b0d91955d9ebf90
xhuve <xhuvelialbana@gmail.com>
2024-07-24 13:05:44 +0200

    Finish

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,7 +5,7 @@ module.exports = async function handleProfileSignup() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    console.log(photo.body + user.firstName + user.lastName);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (error) {
     console.log('Signup system offline');
   }
