b395b32261425972040755d28efba44be818b808
xhuve <xhuvelialbana@gmail.com>
2024-07-24 18:41:00 +0200

    Finish

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error);
   } finally {
     queue.push('Guardrail was processed');
   }
