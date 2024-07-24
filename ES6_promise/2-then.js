1b2673d23f5f1c3b6757be0e479fd61f602bb577
xhuve <xhuvelialbana@gmail.com>
2024-07-24 12:16:49 +0200

    Finish

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 module.exports = function handleResponseFromAPI(promise) {
   promise.then(() => ({
     status: 200,
-    body: success,
+    body: 'success',
   })).catch(() => new Error()).finally(() => {
+    // eslint-disable-next-line no-console
     console.log('Got a response from the API');
   });
 };
