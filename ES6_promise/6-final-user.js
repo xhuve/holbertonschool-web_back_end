92caa83467499d5fa9f5ec5e882561e03aeea0c8
xhuve <xhuvelialbana@gmail.com>
2024-07-24 15:03:10 +0200

    Finish

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,12 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const result = await signUpUser(firstName, lastName)
+    await uploadPhoto(fileName)
+    console.log(`${result.firstName} ${lastName}`)
+  } catch (error) {
+    console.log(error.message)
+  }
 }
