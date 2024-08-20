c78085af98db2a182e082c61beacbcffee515706
xhuve <xhuvelialbana@gmail.com>
2024-08-20 15:54:06 +0200

    Finish

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+strongly typed python
+'''
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+    type anotated function element_length that takes a list of strings and returns a list of tuples
+    '''
+    return [(i, len(i)) for i in lst]
