d2b6aa57b36b541be78e711b54e4e95f332afcc1
xhuve <xhuvelialbana@gmail.com>
2024-08-20 15:43:56 +0200

    Finish

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+strongly typed python
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    function that adds 2 values
+    '''
+
+    def function(multiplier, num):
+        return multiplier * num
+    return function
