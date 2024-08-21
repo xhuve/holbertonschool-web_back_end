0ccc177ffa8cfdb68c5d01c27a723587c571bc6e
xhuve <xhuvelialbana@gmail.com>
2024-08-21 10:05:31 +0200

    Finish

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,13 +3,22 @@
 async python
 '''
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_n(max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
         basics of async
     '''
 
-    return asyncio.create_task(task_wait_random(max_delay))
+    return_list = []
+    task_list = []
+    for _ in range(n):
+        task_list.append(asyncio.create_task(task_wait_random(max_delay)))
+
+    for task in task_list:
+        return_list.append(await task)
+
+    return sorted(return_list)
