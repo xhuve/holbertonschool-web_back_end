25991127647dfc0fb68d0060f547e5133960790a
xhuve <xhuvelialbana@gmail.com>
2024-08-21 09:36:28 +0200

    Finish

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+async python
+'''
+import time
+import asyncio
+
+wait_n  = __import__('1-concurrent_coroutines').wait_n 
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''
+        basics of async
+    '''
+    start = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    total_time = time.time() - start
+
+    return total_time / n
