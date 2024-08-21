4efd728833e02d2239364389d76782f7602a2846
xhuve <xhuvelialbana@gmail.com>
2024-08-21 08:10:08 +0200

    Finish

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ async def wait_random(max_delay=10):
     '''
         basics of async
     '''
-    delay_time = random.random() * max_delay
+    delay_time = random.uniform(0, float(max_delay))
     await asyncio.sleep(delay_time)
 
     return delay_time
