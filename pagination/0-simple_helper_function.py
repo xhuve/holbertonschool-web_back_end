c13a44bfbdc7e5a03af19daae8f27398510b6361
xhuve <xhuvelialbana@gmail.com>
2024-08-21 15:23:17 +0200

    Finish

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 """
 learning pagination through python
 """
 
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int]:
     """
     a function that returns the index of
     a given page number, given the item page size
