81cbb4067f3b32a5b929d3bce8650e69cebd8837
xhuve <xhuvelialbana@gmail.com>
2024-08-23 13:01:36 +0200

    Finish

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Python - Async Comprehension
+Module
+'''
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float]:
+    for _ in range(10):
+        asyncio.sleep(1)
+        yield uniform(1, 10)
