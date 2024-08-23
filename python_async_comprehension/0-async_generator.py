aa25030f377cab0d182a6d993f813e5b2c364ed2
xhuve <xhuvelialbana@gmail.com>
2024-08-23 13:17:37 +0200

    Finish

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,6 +9,9 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, any, any]:
+    '''
+        fuction that yields a random number per second
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(1, 10)
