eec7c0b8d7518c217955ab8f9d463bfb3ed8e4ce
xhuve <xhuvelialbana@gmail.com>
2024-08-24 23:01:03 +0200

    Finish

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,12 @@
 Python - Async Comprehension
 Module
 '''
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     '''
         fuction that returns a random number per second
     '''
