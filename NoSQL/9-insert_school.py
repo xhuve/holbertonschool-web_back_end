d1ce9e433ee59db7bbf85c4124c164bdf997ecf6
xhuve <xhuvelialbana@gmail.com>
2024-08-24 22:32:53 +0200

    Finish

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+insert data from mongodb
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''function to insert data from a NoSQL db'''
+    new_rec = mongo_collection.insert(kwargs)
+    return new_rec._id
