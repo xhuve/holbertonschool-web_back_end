71dc421e0eba7760e1a6c4ed122d298e30e838cb
xhuve <xhuvelialbana@gmail.com>
2024-09-22 20:04:51 +0200

    Finish

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("readable", function() {
+process.stdin.on("readable", () => {
   const stuName = process.stdin.read();
   if (stuName !== null) {
-    process.stdout.write("Your name is: " + stuName);
+    process.stdout.write("Your name is: ", stuName);
   }
   process.stdin.resume();
 });
 
-process.stdin.on("end", function() {
+process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
 });
