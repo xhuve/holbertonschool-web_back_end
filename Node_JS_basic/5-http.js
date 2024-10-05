c21657bd102d50c55d696800c99fbb57f7aacdc7
xhuve <xhuvelialbana@gmail.com>
2024-10-05 11:27:55 +0200

    Fix deprecated module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,42 +1,39 @@
 const http = require('http');
-const url = require('url');
+const { URL } = require('url');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
 
   if (parsedUrl.pathname === '/') {
-    // For the root path ('/'), display 'Hello Holberton School!'
     res.end('Hello Holberton School!');
   } else if (parsedUrl.pathname === '/students') {
     try {
-      const databaseFile = process.argv[2] || 'database.csv'; // // Get the database file path
-
-      let consoleOutput = ''; // variable to capture console.log output
+      const databaseFile = process.argv[2] || 'database.csv'; 
+      let consoleOutput = ''; 
       const originalConsoleLog = console.log;
       console.log = (message) => {
-        consoleOutput += `${message}\n`; //  // Capture console.log output
+        consoleOutput += `${message}\n`; 
       };
 
-      await countStudents(databaseFile); // Retrieve student data with countStudents() function
+      await countStudents(databaseFile); 
 
-      consoleOutput = consoleOutput.trim(); // // Remove extra newline from the end
+      consoleOutput = consoleOutput.trim(); 
 
-      console.log = originalConsoleLog; // // Restore the original console.log function
+      console.log = originalConsoleLog; 
 
       res.end(`This is the list of our students\n${consoleOutput}`);
     } catch (error) {
-      // Handle errors by sending an error response
+      
       res.statusCode = 400;
       res.end(`This is the list of our students\n${error.message}`);
     }
   } else {
-    // Handle other paths with a 'Not Found' response
     res.statusCode = 404;
     res.end('Not Found\n');
   }
 });
 
-app.listen(1245); // Listen on port 1245
+app.listen(1245); 
 
 module.exports = app;
